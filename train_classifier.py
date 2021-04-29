2b970137d0990f6ddf9808e54ba00bae435c0494
moritz <wolter@cs.uni-bonn.de>
2021-04-29 16:04:58 +0200

    argparse

--- train_classifier.py
+++ train_classifier.py
@@ -1,5 +1,6 @@
 import torch
 import pickle
+import argparse
 import numpy as np
 from src.wavelet_math import compute_pytorch_packet_representation_2d
 from src.data_loader import LoadNumpyDataset
@@ -22,18 +23,30 @@ class Regression(torch.nn.Module):
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Train an image classifier')
+    parser.add_argument('--features', choices=['raw', 'packets'],
+                        default='packets',
+                        help='the representation type')
+    parser.add_argument('--batch-size', type=int, default=128,
+                        help='input batch size for testing (default: 128)')
+    args = parser.parse_args()
+    print(args)
+
     train_data_set = LoadNumpyDataset('./data_raw_train')
     val_data_set = LoadNumpyDataset('./data_raw_val')
     train_data_loader = DataLoader(
-        train_data_set, batch_size=1024, shuffle=True,
-        num_workers=1, pin_memory=True)
+        train_data_set, batch_size=128, shuffle=True,
+        num_workers=2)
     val_data_loader = DataLoader(
-        val_data_set, batch_size=1024, shuffle=False,
-        num_workers=1, pin_memory=True)
+        val_data_set, batch_size=128, shuffle=False,
+        num_workers=2)
 
-    packets = False
+    if args.features == 'packets':
+        packets = True
+    else:
+        packets = False
     epochs = 4
-    test_list = []
+    validation_list = []
     loss_list = []
     accuracy_list = []
     step_total = 0
@@ -54,7 +67,6 @@ if __name__ == '__main__':
             optimizer.zero_grad()
             batch_images = batch['image'].cuda(non_blocking=True)
             batch_labels = batch['label'].cuda(non_blocking=True)
-            # batch_labels = torch.nn.functional.one_hot(batch_labels)
             batch_images = (batch_images - 112.52875) / 68.63312
             if packets:
                 channel_list = []
@@ -69,41 +81,56 @@ if __name__ == '__main__':
             loss = loss_fun(torch.squeeze(out), batch_labels)
             ok_mask = torch.eq(torch.max(out, dim=-1)[1], batch_labels)
             acc = torch.sum(ok_mask.type(torch.float32)) / len(batch_labels)
-            print('e', e, 'it', it, 'loss', loss.item(), 'acc', acc.item())
+
+            if it % 10 == 0:
+                print('e', e, 'it', it, 'loss', loss.item(), 'acc', acc.item())
             loss.backward()
             optimizer.step()
             step_total += 1
             loss_list.append((step_total, loss.item()))
             accuracy_list.append([step_total, acc.item()])
 
-        # iterate over val batches.
-        print('testing...')
-        with torch.no_grad():
-            test_total = 0
-            test_ok = 0
-            for test_batch in enumerate(iter(val_data_loader)):
-                batch_images = batch['image'].cuda(non_blocking=True)
-                batch_labels = batch['label'].cuda(non_blocking=True)
-                # batch_labels = torch.nn.functional.one_hot(batch_labels)
-                batch_images = (batch_images - 112.52875) / 68.63312
-                if packets:
-                    channel_list = []
-                    for channel in range(3):
-                        channel_list.append(
-                            compute_pytorch_packet_representation_2d(
-                                batch_images[:, :, :, channel],
-                                wavelet_str=wavelet, max_lev=max_lev))
-                    batch_images = torch.stack(channel_list, -1)
+            # iterate over val batches.
+            if step_total % 500 == 0:
+                print('validating....')
+                with torch.no_grad():
+                    test_total = 0
+                    test_ok = 0
+                    for test_batch in enumerate(iter(val_data_loader)):
+                        batch_images = batch['image'].cuda(non_blocking=True)
+                        batch_labels = batch['label'].cuda(non_blocking=True)
+                        # batch_labels = torch.nn.functional.one_hot(batch_labels)
+                        batch_images = (batch_images - 112.52875) / 68.63312
+                        if packets:
+                            channel_list = []
+                            for channel in range(3):
+                                channel_list.append(
+                                    compute_pytorch_packet_representation_2d(
+                                        batch_images[:, :, :, channel],
+                                        wavelet_str=wavelet, max_lev=max_lev))
+                            batch_images = torch.stack(channel_list, -1)
 
-                out = model(batch_images)
-                loss = loss_fun(torch.squeeze(out), batch_labels)
-                ok_mask = torch.eq(torch.max(out, dim=-1)[1], batch_labels)
-                test_ok += torch.sum(ok_mask).item()
-                test_total += batch_labels.shape[0]
-            test_list.append((step_total, test_ok / test_total))
-            print('test acc', test_list[-1],
-                  'test_ok', test_ok,
-                  'total', test_total)
-    print(test_list)
-    with open('./log/' + 'packets' + str(packets) + '.pkl', 'wb') as f:
-        pickle.dump([loss_list, accuracy_list, test_list], f)
+                        out = model(batch_images)
+                        loss = loss_fun(torch.squeeze(out), batch_labels)
+                        ok_mask = torch.eq(torch.max(out, dim=-1)[1], batch_labels)
+                        test_ok += torch.sum(ok_mask).item()
+                        test_total += batch_labels.shape[0]
+                    validation_list.append((step_total, test_ok / test_total))
+                    print('test acc', validation_list[-1],
+                          'test_ok', test_ok,
+                          'total', test_total)
+    print(validation_list)
+
+    stats_file = './log/' + 'packets' + str(packets) + '.pkl'
+    try:
+        res = pickle.load(open(stats_file, "rb"))
+    except (OSError, IOError) as e:
+        res = []
+        print(e, 'stats.pickle does not exist, \
+              creating a new file.')
+
+        res.append({'train loss': loss_list,
+                    'train acc': accuracy_list,
+                    'val acc': validation_list})
+        pickle.dump(res, open(stats_file, "wb"))
+        print(stats_file, ' saved.')
