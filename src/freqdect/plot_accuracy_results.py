24c629fea864f60ce52e8b627f8b01d2e7ce664c
moritz <wolter@cs.uni-bonn.de>
2021-05-08 01:49:07 +0200

    add scripts and logs.

--- src/freqdect/plot_accuracy_results.py
+++ src/freqdect/plot_accuracy_results.py
@@ -32,6 +32,13 @@ def plot_mean_std(steps, mean, std, color, label='', marker='.'):
                      color=color, alpha=0.2)
 
 
+def get_test_acc_mean_std(dict_list: dict, key: str):
+    test_accs = []
+    for experiment_dict in dict_list:
+        test_accs.append(experiment_dict[key])
+    return np.mean(test_accs), np.std(test_accs)
+
+
 def main():
     raw_logs = pickle.load(open('./log/packetsFalse.pkl', 'rb'))
     packet_logs = pickle.load(open('./log/packetsTrue.pkl', 'rb'))
@@ -47,13 +54,18 @@ def main():
     plot_mean_std(steps, mean, std, color=colors[1],
                   label='packet validation acc')
 
+    pt_mean, pt_std = get_test_acc_mean_std(packet_logs, 'test_acc')
+    rt_mean, rt_std = get_test_acc_mean_std(raw_logs, 'test_acc')
+    plt.errorbar(steps[-1], pt_mean, pt_std, color=colors[2],
+                 label='packet test acc', marker='.')
+    plt.errorbar(steps[-1], rt_mean, rt_std, color=colors[3],
+                 label='raw test acc', marker='.')
+
     plt.ylabel('mean accuracy')
     plt.xlabel('training steps')
     plt.title('Validation accuracy FFHQ-StyleGAN')
     plt.legend()
     plt.show()
-
-    print('stop')
 
 
 if __name__ == '__main__':
---
+++ src/freqdect/plot_mean_packets.py
@@ -0,0 +1,16 @@
+import torch
+import numpy as np
+
+
+def main():
+    import matplotlib.pyplot as plt
+    from torch.utils.data import DataLoader
+
+    # raw images - use only the training set.
+    train_packet_set = LoadNumpyDataset('./data/source_data_packets_train')
+
+    
+
+
+if __name__ == '__main__':
+    main()
