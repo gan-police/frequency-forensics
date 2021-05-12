17cee78e12ca5ce59f4df3c7725d2c5a42f48c79
Felix Blanke <felix.blanke@scai.fraunhofer.de>
2021-05-12 17:55:46 +0200

    Copying files as .tar to tmp dir for better performance

--- scripts/prepare_dataset.sh
+++ scripts/prepare_dataset.sh
@@ -10,8 +10,19 @@
 #SBATCH --gres gpu:v100:1
 #SBATCH --cpus-per-task=2
 
-DATASET="../datasets/lsun_bedroom_200k_png"
+#DATASET="../datasets/lsun_bedroom_200k_png"
 ANACONDA_ENV="~/myconda-env"
+
+DATASETS="/home/ndv/projects/wavelets/datasets"
+DATESET="lsun_bedroom_200k_png"
+
+if [ -f ${DATASETS}/${DATASET}.tar ]; then
+  echo "Tarred input folder exists, copying to $TMPDIR"
+  cp "${DATASETS}/${DATASET}.tar" $TMPDIR/
+  cd $TMPDIR
+  tar xf ${DATASET}.tar
+  DATASET="${TMPDIR}/$DATASET"
+fi
 
 module load PyTorch
 module load Pillow
@@ -26,3 +37,8 @@ python -m freqdect.prepare_dataset_batched $DATASET \
   --test-size 30000 \
   --val-size 20000 \
   -p
+
+#if [ -d "${TMPDIR}/${DATASET}_raw_train" ]; then
+if ls "${TMPDIR}/${DATASET}_*" > /dev/null 2>&1; then
+  cp ""${TMPDIR}/${DATASET}_*" "${DATASETS}"
+fi
