e526d92d96bead52d8c9d887d8f4f9592bf71191
Felix Blanke <felix.blanke@scai.fraunhofer.de>
2021-05-12 22:09:25 +0200

    Copying dataset to tmp dir at gpu node for better performance

--- scripts/train_lsun.sh
+++ scripts/train_lsun.sh
@@ -10,9 +10,33 @@
 #SBATCH --gres gpu:v100:1
 #SBATCH --cpus-per-task=16
 
-DATASET_RAW="../datasets/lsun_bedroom_200k_png_raw"
-DATASET_PACKETS="../datasets/lsun_bedroom_200k_png_packets"
+DATASETS="/home/ndv/projects/wavelets/datasets"
+DATASET_RAW="lsun_bedroom_200k_png_raw"
+DATASET_PACKETS="lsun_bedroom_200k_png_packets"
 ANACONDA_ENV="~/myconda-env"
+
+RAW_PREFIX=$DATASETS
+PACKETS_PREFIX=$DATASETS
+
+LOG_DIR="log"
+TARGET_LOG_DIR="/home/ndv/projects/wavelets/log"
+
+
+if [ -f ${DATASETS}/{DATASET_RAW}.tar ]; then
+  echo "Tarred raw input folder exists, copying to $TMPDIR"
+  cp "${DATASETS}/${DATASET_RAW}.tar" "${TMPDIR}"
+  cd $TMPDIR
+  tar -xf "${DATASET_RAW}.tar"
+  RAW_PREFIX="${TMPDIR}/${DATASET_RAW}"
+fi
+
+if [ -f ${DATASETS}/{DATASET_PACKETS}.tar ]; then
+  echo "Tarred packets input folder exists, copying to $TMPDIR"
+  cp "${DATASETS}/${DATASET_PACKETS}.tar" "${TMPDIR}"
+  cd $TMPDIR
+  tar -xf "${DATASET_PACKETS}.tar"
+  PACKETS_PREFIX="${TMPDIR}/${DATASET_PACKETS}"
+fi
 
 module load CUDA
 module load Anaconda3
@@ -23,23 +47,25 @@ pip install -q -e .
 
 for i in 0 1 2 3 4
 do
-   echo "packet experiment no: $i "
-   python -m freqdect.train_classifier \
-	--features packets \
-	--seed $i \
-	--data-prefix $DATASET_PACKETS \
-	--nclasses 4 \
-	--calc-normalization
+  echo "packet experiment no: $i "
+  python -m freqdect.train_classifier \
+	  --features packets \
+	  --seed $i \
+	  --data-prefix $PACKETS_PREFIX \
+	  --nclasses 4 \
+	  --calc-normalization
 done
 
 for i in 0 1 2 3 4
 do
-   echo "raw experiment no: $i "
-   python -m freqdect.train_classifier \
-        --features raw \
-        --seed $i \
-        --data-prefix $DATASET_RAW \
-        --nclasses 4 \
-        --calc-normalization
+  echo "raw experiment no: $i "
+  python -m freqdect.train_classifier \
+    --features raw \
+    --seed $i \
+    --data-prefix $RAW_PREFIX \
+    --nclasses 4 \
+    --calc-normalization
 done
 
+# copy log files back to home dir
+cp "${TMPDIR}/${LOG_FILES}/*" "${TARGET_DIR}"
