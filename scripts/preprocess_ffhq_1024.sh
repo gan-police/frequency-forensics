46505761258e74c05891793d26b021c6659a86de
moritz <wolter@cs.uni-bonn.de>
2021-05-14 23:01:44 +0200

    ffhq 1024.

--- scripts/preprocess_ffhq_1024.sh
+++ scripts/preprocess_ffhq_1024.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=1 python src/freqdect/prepare_dataset_batched.py ./data/ffhq_stylegan_large --batch-size 128 --train-size 10000 --val-size 500 --test-size 2000 --packets
+CUDA_VISIBLE_DEVICES=1 python src/freqdect/prepare_dataset_batched.py ./data/ffhq_stylegan_large --batch-size 100 --train-size 60000 --val-size 2000 --test-size 8000 --packets
