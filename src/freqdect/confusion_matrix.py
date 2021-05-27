c5ed1492eb1b14137eee4a61d83a364030bbaad1
Felix Blanke <felix.blanke@scai.fraunhofer.de>
2021-05-27 23:40:20 +0200

    Calculating confusion matrix for binary real/fake detection

--- src/freqdect/confusion_matrix.py
+++ src/freqdect/confusion_matrix.py
@@ -1,4 +1,5 @@
 import argparse
+from collections import defaultdict
 import torch
 import numpy as np
 from torch.utils.data import DataLoader
@@ -22,12 +23,10 @@ def calculate_confusion_matrix(args):
         test_data_set, batch_size=args.batch_size, shuffle=False, num_workers=2
     )
 
-    n_classes = len(set(test_data_set.labels))
-
     if args.model == "regression":
-        model = Regression(n_classes).cuda()
+        model = Regression(args.nclasses).cuda()
     else:
-        model = CNN(n_classes, args.features == 'packets').cuda()
+        model = CNN(args.nclasses, args.features == 'packets').cuda()
 
     initialize_model(model, args.classifier_path)
     model.eval()
@@ -43,10 +42,63 @@ def calculate_confusion_matrix(args):
             out = model(batch_images)
             out_labels = torch.max(out, dim=-1)[1]
 
+            if args.nclasses == 2:
+                batch_labels[batch_labels > 0] = 1
+
             correct_labels.extend(batch_labels.cpu())
             predicted_labels.extend(out_labels.cpu())
 
     return confusion_matrix(correct_labels, predicted_labels)
+
+
+def confusion_matrix_generalized(args):
+    if args.normalize:
+        num_of_norm_vals = len(args.normalize)
+        assert num_of_norm_vals == 2 or num_of_norm_vals == 6
+        mean = torch.tensor(args.normalize[: num_of_norm_vals // 2])
+        std = torch.tensor(args.normalize[(num_of_norm_vals // 2):])
+    else:
+        mean, std = [None, None]
+
+    test_data_set = LoadNumpyDataset(args.data, mean=mean, std=std)
+    test_data_loader = DataLoader(
+        test_data_set, batch_size=args.batch_size, shuffle=False, num_workers=2
+    )
+
+    if args.model == "regression":
+        model = Regression(args.nclasses).cuda()
+    else:
+        model = CNN(args.nclasses, args.features == 'packets').cuda()
+
+    initialize_model(model, args.classifier_path)
+    model.eval()
+
+    predicted_dict = defaultdict(list)
+
+    label_names = np.array(["Original", "CramerGAN", "MMDGAN", "ProGAN", "SNGAN"])
+
+    with torch.no_grad():
+        for test_batch in iter(test_data_loader):
+            batch_images = test_batch["image"].cuda(non_blocking=True)
+            batch_labels = test_batch["label"].cpu()
+
+            batch_names = label_names[batch_labels]
+
+            out = model(batch_images)
+            out_labels = torch.max(out, dim=-1)[1].cpu()
+
+            for k, v in zip(batch_names, out_labels):
+                predicted_dict[k].append(v)
+
+    matrix = np.zeros((len(label_names), args.nclasses), dtype=int)
+
+    for label_idx, label in enumerate(label_names):
+        predicted_labels = np.array(predicted_dict[label])
+
+        for class_idx in range(args.nclasses):
+            matrix[label_idx, class_idx] = len(predicted_labels[predicted_labels == class_idx])
+
+    return matrix
 
 
 if __name__ == "__main__":
@@ -91,27 +143,37 @@ if __name__ == "__main__":
         action="store_true",
         help="plot the confusion matrix and store the plot as png"
     )
+    parser.add_argument(
+        "--nclasses", type=int, default=2, help="number of classes (default: 2)"
+    )
+    parser.add_argument(
+        "--generalized", action="store_true"
+    )
     args = parser.parse_args()
 
-    confusion_matrix = calculate_confusion_matrix(args)
+    if args.generalized:
+        matrix = confusion_matrix_generalized(args)
+        print(matrix)
+    else:
+        confusion_matrix = calculate_confusion_matrix(args)
 
-    print('accuracy: ', np.trace(confusion_matrix)/confusion_matrix.sum())
+        print('accuracy: ', np.trace(confusion_matrix)/confusion_matrix.sum())
 
-    label_names = ["Original", "CramerGAN", "MMDGAN", "ProGAN", "SNGAN"]
+        label_names = ["Original", "CramerGAN", "MMDGAN", "ProGAN", "SNGAN"]
 
-    diag = np.diag(confusion_matrix)
+        diag = np.diag(confusion_matrix)
 
-    worst_index = np.argmin(diag)
-    best_index = np.argmax(diag)
-    print(f"worst index: {worst_index} ({label_names[worst_index]}) with an accuracy of {diag[worst_index]/confusion_matrix[worst_index].sum()*100:.2f}%")
-    print(f"best index: {best_index} ({label_names[best_index]}) with an accuracy of {diag[best_index]/confusion_matrix[best_index].sum()*100:.2f}%")
+        worst_index = np.argmin(diag)
+        best_index = np.argmax(diag)
+        print(f"worst index: {worst_index} ({label_names[worst_index]}) with an accuracy of {diag[worst_index]/confusion_matrix[worst_index].sum()*100:.2f}%")
+        print(f"best index: {best_index} ({label_names[best_index]}) with an accuracy of {diag[best_index]/confusion_matrix[best_index].sum()*100:.2f}%")
 
-    print(confusion_matrix)
+        print(confusion_matrix)
 
-    if args.plot:
-        import matplotlib.pyplot as plt
+        if args.plot:
+            import matplotlib.pyplot as plt
 
-        disp = ConfusionMatrixDisplay(confusion_matrix=confusion_matrix, display_labels=label_names)
-        disp.plot()
-        plt.savefig('confusion_matrix.png')
-        plt.show()
+            disp = ConfusionMatrixDisplay(confusion_matrix=confusion_matrix, display_labels=label_names)
+            disp.plot()
+            plt.savefig('confusion_matrix.png')
+            plt.show()
