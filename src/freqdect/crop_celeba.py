6e054559412188fe78c992b9a0ea773c8861abab
Felix Blanke <felix.blanke@scai.fraunhofer.de>
2021-06-04 11:59:28 +0200

    blackify cropping code

--- src/freqdect/crop_celeba.py
+++ src/freqdect/crop_celeba.py
@@ -8,18 +8,23 @@ import numpy as np
 
 from concurrent.futures import ProcessPoolExecutor
 
+
 def crop_image(stupid):
     i, directory, file_path, output = stupid
-    if file_path.endswith("png") or file_path.endswith("jpeg") or file_path.endswith("jpg"):
+    if (
+        file_path.endswith("png")
+        or file_path.endswith("jpeg")
+        or file_path.endswith("jpg")
+    ):
         image = np.asarray(Image.open(f"{directory}/{file_path}"))
 
         if image.shape[0] != 128 or image.shape[1] != 128:
-            x,y,_= image.shape
+            x, y, _ = image.shape
             image = np.copy(image)
-            x_upper = min(121+64, x)
-            y_upper = min(89+64, y)
-            image = image[x_upper-128:x_upper, y_upper-128:y_upper]
-            image = np.clip(image, 0, 255.).astype(np.uint8)
+            x_upper = min(121 + 64, x)
+            y_upper = min(89 + 64, y)
+            image = image[x_upper - 128 : x_upper, y_upper - 128 : y_upper]
+            image = np.clip(image, 0, 255.0).astype(np.uint8)
 
         if not (image.shape[0] == 128 and image.shape[1] == 128):
             print("Aborting")
@@ -28,10 +33,11 @@ def crop_image(stupid):
         Image.fromarray(image).save(f"{output}/celeba_{file_path}")
         return i
 
+
 def main(args):
     os.makedirs(args.OUTPUT, exist_ok=True)
-    paths = os.listdir(args.DIRECTORY)[:args.SIZE]
-    packed = map(lambda x: (x[0], args.DIRECTORY, x[1], args.OUTPUT) , enumerate(paths))
+    paths = os.listdir(args.DIRECTORY)[: args.SIZE]
+    packed = map(lambda x: (x[0], args.DIRECTORY, x[1], args.OUTPUT), enumerate(paths))
 
     with ProcessPoolExecutor() as pool:
         jobs = pool.map(crop_image, packed)
@@ -45,6 +51,7 @@ def parse_args():
     parser.add_argument("SIZE", help="Amount of data to convert.", type=int)
 
     return parser.parse_args()
+
 
 if __name__ == "__main__":
     main(parse_args())
--- src/freqdect/crop_lsun.py
+++ src/freqdect/crop_lsun.py
@@ -12,23 +12,42 @@ def transform_image(stupid):
     file_path, directory, output = stupid
     # catch errors and continue with different files
     try:
-        if file_path.endswith("png") or file_path.endswith("jpeg") or file_path.endswith("jpg") or file_path.endswith("webp"):
+        if (
+            file_path.endswith("png")
+            or file_path.endswith("jpeg")
+            or file_path.endswith("jpg")
+            or file_path.endswith("webp")
+        ):
             image = Image.open(f"{directory}/{file_path}")
             x, y = image.size
             if y < x:
                 crop_height = y
                 crop_width = y
 
-                crop_left = (x - y)//2
+                crop_left = (x - y) // 2
                 crop_top = 0
-                image = image.crop((crop_left, crop_top, crop_left+crop_width, crop_top+crop_height))
+                image = image.crop(
+                    (
+                        crop_left,
+                        crop_top,
+                        crop_left + crop_width,
+                        crop_top + crop_height,
+                    )
+                )
             elif x < y:
                 crop_height = x
                 crop_width = x
 
                 crop_left = 0
-                crop_top = (y - x)//2
-                image = image.crop((crop_left, crop_top, crop_left+crop_width, crop_top+crop_height))
+                crop_top = (y - x) // 2
+                image = image.crop(
+                    (
+                        crop_left,
+                        crop_top,
+                        crop_left + crop_width,
+                        crop_top + crop_height,
+                    )
+                )
 
             image = image.resize((128, 128))
 
@@ -41,14 +60,16 @@ def transform_image(stupid):
     except ValueError as exc:
         print(file_path, exc, x, y)
 
+
 def main(args):
     os.makedirs(args.OUTPUT, exist_ok=True)
 
     # only consider the specified number of files
-    paths = os.listdir(args.DIRECTORY)[:args.SIZE]
+    paths = os.listdir(args.DIRECTORY)[: args.SIZE]
     packed = map(lambda p: (p, args.DIRECTORY, args.OUTPUT), paths)
     with ProcessPoolExecutor() as pool:
         list(pool.map(transform_image, packed))
+
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -60,6 +81,7 @@ def parse_args():
     parser.add_argument("SIZE", help="Amount of data to convert.", type=int)
 
     return parser.parse_args()
+
 
 if __name__ == "__main__":
     main(parse_args())
