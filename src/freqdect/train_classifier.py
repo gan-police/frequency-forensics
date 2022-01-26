9d3dac2d15da9883e2d2f5cb9b49a62bd0bfc608
Felix Blanke <felix.blanke@scai.fraunhofer.de>
2022-01-26 23:58:51 +0100

    Also log training accuracy in tensorboard

--- src/freqdect/train_classifier.py
+++ src/freqdect/train_classifier.py
@@ -298,6 +298,7 @@ def main():
 
             if args.tensorboard:
                 writer.add_scalar("loss/train", loss.item(), step_total)
+                writer.add_scalar("accuracy/train", acc.item(), step_total)
                 if it == 0:
                     writer.add_graph(model, batch_images)
 
